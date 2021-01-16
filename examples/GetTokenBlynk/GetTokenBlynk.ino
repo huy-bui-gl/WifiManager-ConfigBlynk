8260011b5a5dcb3606391ae24acd315399277bb5
Huy Bui V <huy.bui@monstar-lab.com>
2021-01-16 14:18:31 +0700

    change readme

--- examples/GetTokenBlynk/GetTokenBlynk.ino
+++ examples/GetTokenBlynk/GetTokenBlynk.ino
@@ -1,27 +1,35 @@
 /**************************************************************
-   
+
    Write by Bui Van Huy
 
  **************************************************************/
 
-#include <ESP8266WiFi.h>         
+#include <ESP8266WiFi.h>
 
 //needed for library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>       
+#include <WiFiManager.h>
 
+#define  FLASH_BUTTON 0
 
 void setup() {
-    
-    Serial.begin(115200);
-    WiFiManager wifiManager;
-    wifiManager.autoConnect("CONFIG");
-    Serial.println("connected...yeey :)");
+  pinMode(FLASH_BUTTON, INPUT_PULLUP);
 
-    //getToken In memory
-    Serial.print("token = "); Serial.println(wifiManager.getToken());
+
+  Serial.begin(115200);
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("CONFIG");
+  Serial.println("connected...yeey :)");
+
+  //getToken In memory
+  Serial.print("token = "); Serial.println(wifiManager.getToken());
 }
 
 void loop() {
+  if (!digitalRead(FLASH_BUTTON)) {
+    delay(500);
+    WiFiManager wifiManager;
+    wifiManager.startConfigPortal("CONFIG");
+  }
 }
